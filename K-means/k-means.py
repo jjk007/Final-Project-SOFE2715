c8140b223398ab42c2c0b301d816c8e018d02225
Justin Kaipada <Justin Kaipada>
2017-02-12 23:03:28 -0500

    Began the skeletan

---
+++ K-means/k-means.py
@@ -0,0 +1,42 @@
+import csv
+import random
+import matplotlib
+
+class Kmeans(object):
+    "K-means clustering algorithm"
+
+    def __init__(self, unclusteredData=[]):
+        self.data = unclusteredData
+
+    def cluster(self):
+        "This is the main method, which executes k-means clustering algorithm"
+        k = 5 # k is the number of clusters to be develped from the data
+        c1 = random.randrange(a,b) # These are the randomly picked centroids
+        c2 = random.randrange(a,b)
+        c3 = random.randrange(a,b)
+        c4 = random.randrange(a,b)
+        c5 = random.randrange(a,b)
+
+        abc = 1+1
+        print abc
+        return 0
+
+    def parse(self):
+        "Parses the data sets from the csv file we are given to work with"
+        # TODO
+        return 0
+
+    def euclideanDistance(self, xCord, yCord):
+        "This calculates the Euclidean Distance b/w X & Y, in the standard way"
+        distance = xCord + yCord
+        # TODO
+        return distance
+
+    def display(self):
+        "Displays the data, for TESTING"
+        #TODO
+        return 0
+
+
+# inf
+# https://youtu.be/RD0nNK51Fp8
