44b6980c2e418cff889aa2e4c1b08a18793efbb5
Justin K -Linux <justinjoseph0007@gmail.com>
2017-02-13 14:12:51 -0500

    typos

--- K-means/k-means.py
+++ K-means/k-means.py
@@ -3,6 +3,7 @@ import random
 import math
 import matplotlib.pyplot as plt
 
+
 class Kmeans(object):
     "K-means clustering algorithm"
 
@@ -11,19 +12,19 @@ class Kmeans(object):
 
     def cluster(self):
         "This is the main method, which executes k-means clustering algorithm"
-        k = 5 # k is the number of clusters to be develped from the data
+        k = 5  # k is the number of clusters to be develped from the data
         c1 = random.choice(data)# These are the randomly picked centroids
-        c2 = random.choice(data)# data is the list of unclustered data we've got
+        c2 = random.choice(data)# Data is the list of unclustered data we've got
         c3 = random.choice(data)
         c4 = random.choice(data)
         c5 = random.choice(data)
 
     def parse():
         "Parses the data sets from the csv file we are given to work with"
-        file = open("exercise-1.csv")# should be manualized later
-        rawFile = csv.reader(file)   # Reading the csv file into a raw form
-        rawData = list(rawFile)      # Converting the raw data into list from.
-        dataObject = Kmeans(rawData) # Creating the object and passing the data
+        file = open("exercise-1.csv")  # should be manualized later
+        rawFile = csv.reader(file)    # Reading the csv file into a raw form
+        rawData = list(rawFile)       # Converting the raw data into list from.
+        dataObject = Kmeans(rawData)  # Creating the obj and passing the data
         '''
         Remember, csv reader reads the file line by line, and for the files
         that are given to us first line is always meta data and we don't want
@@ -32,7 +33,7 @@ class Kmeans(object):
         Since we have 2 columns, when converted into list/array it is 2D list.
         Here, index 0 in each line is x-coordinate, index 1 is the y-coordinate
         So we can refer to them data[line_number][x/y].
-        We also need to convert them to number everytime.They are read as string
+        We also need to convert them to a number.They are read as string
         '''
 
     def euclideanDistance(self, p, q):
