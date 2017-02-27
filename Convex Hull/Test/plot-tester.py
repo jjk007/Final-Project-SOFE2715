832f05ca15f5c3cb6a76eb7fbd96844ee347907c
Justin K -Linux <justin.kaipada@uoit.net>
2017-02-27 15:17:12 -0500

    So, for convex hull we can use matplotlib

---
+++ Convex Hull/Test/plot-tester.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+dates = [0, 1 , 2, 3, 4, 5]
+dates1 = [2, 4 , 5, 6, 4, 1]
+values = [2, 1, 1, 1, 1, 2]
+values1 = [3, 4, 7, 4, 2, 9]
+
+plt.scatter(dates1, values1)
+plt.plot(dates, values, '-o')
+plt.show()
