bd5172a2c2c414632f33c266f4cda95b78e061a5
Justin K -Linux <justin.kaipada@uoit.net>
2017-02-27 17:32:30 -0500

    Added ressources

--- Convex Hull/Test/plot-tester.py
+++ Convex Hull/Test/plot-tester.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-dates = [0, 1 , 2, 3, 4, 5]
-dates1 = [2, 4 , 5, 6, 4, 1]
+dates = [0, 1, 2, 3, 4, 5]
+dates1 = [2, 4, 5, 6, 4, 1]
 values = [2, 1, 1, 1, 1, 2]
 values1 = [3, 4, 7, 4, 2, 9]
 
