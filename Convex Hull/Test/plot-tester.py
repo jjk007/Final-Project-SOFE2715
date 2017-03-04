3ec0a4f62cf7a1e2669b07a593c2d1251f3a45b4
Justin K -Linux <justin.kaipada@uoit.net>
2017-03-04 12:14:27 -0500

    plot-tester makes a mock graph now

--- Convex Hull/Test/plot-tester.py
+++ Convex Hull/Test/plot-tester.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 
-dates = [0, 1, 2, 3, 4, 5]
-dates1 = [2, 4, 5, 6, 4, 1]
-values = [2, 1, 1, 1, 1, 2]
-values1 = [3, 4, 7, 4, 2, 9]
+x = [1, 3, 7, 6, 2, 1]
+y = [1, 0, 3, 7, 5, 1]
 
-plt.scatter(dates1, values1)
-plt.plot(dates, values, '-o')
+x1 = [2, 4, 6, 4, 3, 5]
+y1 = [3, 4, 4, 2, 1, 4]
+
+plt.plot(x, y, '-o')  # Make the boundaries
+plt.scatter(x1, y1, marker=(5, 2), color="r")
 plt.show()
