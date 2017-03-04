780e1f7f4be13b933c9485e7cbd8182895ad67a4
Justin K -Linux <justinjoseph0007@gmail.com>
2017-03-04 16:36:22 -0500

    Locating the starting point

--- Convex Hull/Convex-hull.py
+++ Convex Hull/Convex-hull.py
@@ -28,15 +28,22 @@ def euclideanDistance(p, q):
     return distance
 
 
+def locate_min(a):
+    smallest = min(a)  # Locating all minmum elements of the passed list
+    return [index for index, element in enumerate(a)
+            if smallest == element]
+
+
 def parse():
     "Parses the data sets from the csv file we are given to work with"
-    file = open("./Exercises/exercise-1.csv")  # should be manualized later
+    file = open("./Exercises/exercise-6.csv")  # should be manualized later
     rawFile = csv.reader(file)    # Reading the csv file into a raw form
     rawData = list(rawFile)       # Converting the raw data into list from.
     return rawData
 
 
 def main():
+    global newData
     start = timer()
     data = parse()                  # Calling the parse funtion we made
     labels = data.pop(0)
@@ -45,9 +52,13 @@ def main():
         newData.append([])          # Add a new sublsit every time
         for j in range(0, 2):       # Append converted data to the new list
             newData[i].append(float(data[i][j]))
+    # Finding the starting point P
+    tempData = toXandY(newData)  # tempData -> [[Xs][Ys]]
+    P = tempData[1].index(min(tempData[1]))  # Locating the minimum y-cord
+
     # Timer
     end = timer()
-    print "Amount of time elapsed: " + str(end-start) + " seconds"
+    print "Amount of time elapsed to service " + labels[0] + " and " + labels[1] + " : " + str(end-start) + " seconds"
 
 
 if __name__ == "__main__":
