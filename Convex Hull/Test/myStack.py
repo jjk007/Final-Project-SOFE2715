8d2a2c2dfbfe740ecf3bdeed71c6dcf77ee5cf26
Justin K -Linux <justinjoseph0007@gmail.com>
2017-03-29 15:02:48 -0400

    Finished implementing stack class

--- Convex Hull/Test/myStack.py
+++ Convex Hull/Test/myStack.py
@@ -1,6 +1,6 @@
 class myStack(object):
 
-    def __inti__(self):
+    def __init__(self):
         self.stackContents = [None]*2000
         self.currentSize = 0
 
@@ -9,14 +9,43 @@ class myStack(object):
         if self.stackContents[0] is None:
             return None
         else:
-            item = self.stackContents[self.currentSize]
-            self.stackContents[self.currentSize] = None
+            item = self.stackContents[self.currentSize-1]
+            self.stackContents[self.currentSize-1] = None
             self.currentSize -= 1
             return item
 
     def stackTop(self):
         "This method returns the top most element of the stack"
-        pass
+        if self.stackContents[0] is None:
+            return None
+        else:
+            item = self.stackContents[self.currentSize-1]
+            return item
 
     def stackPush(self, element):
         "This method inserts element into the current stack"
+        self.stackContents[self.currentSize] = element
+        self.currentSize += 1
+
+    def printStack(self):
+        for i in range(0, self.currentSize):
+            print self.stackContents[i],
+        print ""
+
+    def stackSize(self):
+        return self.currentSize
+
+
+HullX = myStack()
+
+HullX.stackPush(54)
+HullX.stackPush(87)
+HullX.stackPush(856)
+HullX.stackPush(11)
+HullX.stackPush(21)
+HullX.stackPush(35)
+
+HullX.printStack()
+print HullX.stackPop()
+print HullX.stackSize()
+print HullX.stackTop()
